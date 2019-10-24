6287da09749acc6c10b819bbc57e753c3462e3b4
lyj <liuyongjian@digibird.com.cn>
2019-10-24 18:25:23 +0800

    [MJPEG] 添加工程文件

---
+++ MjpegStreamServer/PreviewImageService.cs
@@ -0,0 +1,166 @@
+﻿#region License
+// /////////////////////////////////////////////////////////////////////////////
+// Copyright (c) DIGIBIRD(http://www.digibird.com.cn)
+// 北京小鸟科技股份有限公司保留所有代码著作权，如有任何疑问请访问官方网站与我们联系。
+// 代码只针对特定客户使用，不得在未经允许或授权的情况下对外传播扩散，恶意传播者自行承担法律后果。
+// 本代码仅用于北京小鸟科技股份有限公司的DSOC、DMIS等项目。
+// 
+// 创建日期:     2019年10月24日11:37
+// 修改日期:     2019年10月24日11:37
+// 文件名称:     PreviewImageService.cs
+// 创建作者:     LYJ
+// 类 描 述 :  
+// /////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+
+namespace MjpegStreamServer
+{
+    public class PreviewImageService
+    {
+        private static PreviewImageService instance;
+        private static object lockObj = new object();
+        private bool running;
+        private Thread thread;
+        private List<RestfulScreenData> screenDataList;
+        private Dictionary<int, Bitmap> screenBitmapDict = new Dictionary<int, Bitmap>();
+        public static PreviewImageService GetInstance()
+        {
+            lock (lockObj)
+            {
+                if (instance == null)
+                {
+                    instance = new PreviewImageService();
+                }
+            }
+
+            return instance;
+        }
+
+        public void Init(List<RestfulScreenData> screenNodeList)
+        {
+            screenDataList = screenNodeList;
+            int count = screenNodeList.Count;
+            int previewGridCount = Program.gridX * Program.gridY;
+            int previewTotal = ((count - 1) / previewGridCount) + 1;
+
+            for (int previewIndex=0; previewIndex< previewTotal;previewIndex++)
+            {
+                Bitmap screenBmp = new Bitmap(Program.previewWidth, Program.previewheight);
+                
+                screenBitmapDict.Add(previewIndex,screenBmp);
+            }
+
+            running = true;
+
+            //启动分屏幕抓取任务
+            thread = new Thread(CaptureScreenTask);
+            thread.Start();
+        }
+
+        public void UnInit()
+        {
+            foreach (var keyValuePair in screenBitmapDict)
+            {
+                keyValuePair.Value.Dispose();
+            }
+            screenBitmapDict.Clear();
+
+            thread.Join(10);
+        }
+
+        private void CaptureScreenTask()
+        {
+            while (running)
+            {
+                int previewGridCount = Program.gridX * Program.gridY;
+
+                int previewTotal = ((screenDataList.Count - 1) / previewGridCount) + 1;
+                for (int outputChannel = 0; outputChannel < previewTotal; outputChannel++)
+                {
+                    Bitmap outputPreviewBitmap = GetScreenPreviewBitmap(outputChannel);
+                    lock (outputPreviewBitmap)
+                    {
+                        System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(outputPreviewBitmap);
+
+                        //清空画布并以透明背景色填充
+                        g.Clear(System.Drawing.Color.Transparent);
+
+                        for (int oid = 1; oid <= previewGridCount; oid++)
+                        {
+                            int screenId = oid + outputChannel * previewGridCount;
+
+                            RestfulScreenData restfulScreenData = GetRestfulScreenData(screenId);
+
+                            if (restfulScreenData == null)
+                            {
+                                continue;
+                            }
+
+                            int previewStartId = ((screenId - 1) - outputChannel * previewGridCount);
+                            int targetX = previewStartId % Program.gridX;
+                            int targetY = previewStartId / Program.gridX;
+
+                            Bitmap screenBitmap = ImageHelper.CaptureScreen(new Rectangle(int.Parse(restfulScreenData.x), int.Parse(restfulScreenData.y), int.Parse(restfulScreenData.width), int.Parse(restfulScreenData.height)));
+
+                            //设置高质量插值法
+                            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+
+                            //设置高质量,低速度呈现平滑程度
+                            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                            Rectangle targetRectangle = new System.Drawing.Rectangle(
+                                targetX * Program.previewWidth / Program.gridX,
+                                targetY * Program.previewheight / Program.gridY, Program.previewWidth / Program.gridX,
+                                Program.previewheight / Program.gridY);
+
+                            //在指定位置并且按指定大小绘制原图片的指定部分
+                            g.DrawImage(screenBitmap, targetRectangle, new System.Drawing.Rectangle(0, 0, screenBitmap.Width, screenBitmap.Height), System.Drawing.GraphicsUnit.Pixel);
+
+                            Console.WriteLine("###CaptureScreenTask previewStartId " + previewStartId + " targetRectangle.x " + targetRectangle.X + " targetRectangle.y " + targetRectangle.Y);
+
+                            //是否分屏幕图片内存
+                            screenBitmap.Dispose();
+
+                        }
+
+                        g.Dispose();
+                    }
+                }
+
+                Thread.Sleep(1000 / Program.frame);
+            }
+        }
+
+        public Bitmap GetScreenPreviewBitmap(int channel)
+        {
+            Bitmap screenBitmap;
+
+            lock (lockObj)
+            {
+                screenBitmapDict.TryGetValue(channel, out screenBitmap);
+            }
+          
+            return screenBitmap;
+        }
+
+        public RestfulScreenData GetRestfulScreenData(int screenId)
+        {
+            RestfulScreenData screenData = null;
+            foreach (var restfulScreenData in screenDataList)
+            {
+                if (int.Parse(restfulScreenData.id) == screenId)
+                {
+                    screenData = restfulScreenData;
+                    break;
+                }
+            }
+
+            return screenData;
+        }
+    }
+}
---
+++ MjpegStreamServer/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml;
+
+/// <summary>
+//  http://192.168.2.107:9191/preview/0/normal
+/// </summary>
+namespace MjpegStreamServer
+{
+    public class RestfulScreenData
+    {
+        [DefaultValue("0")]
+        public string id { get; set; }
+        [DefaultValue("0")]
+        public string x { get; set; }
+        [DefaultValue("0")]
+        public string y { get; set; }
+        [DefaultValue("0")]
+        public string width { get; set; }
+        [DefaultValue("0")]
+        public string height { get; set; }
+    }
+
+    class Program
+    {
+        public static string ServerIp = "";
+        public static int port = 9191;
+        public static int gridX = 2;
+        public static int gridY = 2;
+        public static int previewWidth = 1920;
+        public static int previewheight = 1080;
+        public static int frame = 10;
+        public static List<RestfulScreenData> screenNodeList = new List<RestfulScreenData>();
+        static void Main(string[] args)
+        {
+            string configXml = "./AppScreensSettings.xml";
+
+            if (args.Length >= 7)
+            {
+                port = int.Parse(args[0]);
+                gridX = int.Parse(args[1]);
+                gridY = int.Parse(args[2]);
+                previewWidth = int.Parse(args[3]);
+                previewheight = int.Parse(args[4]);
+                frame = int.Parse(args[5]);
+                configXml = args[6];
+            }
+            else
+            {
+                //warnning
+            }
+
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(configXml);
+            XmlElement root = xmlDoc.DocumentElement;
+            XmlNodeList xmlScreenNodeList = root.SelectNodes("/AppSettings/Screes/Screen");
+            foreach (XmlNode screenNode in xmlScreenNodeList)
+            {
+                XmlNode curXmlNode = screenNode.FirstChild;
+                string id = curXmlNode.InnerText;
+                curXmlNode = curXmlNode.NextSibling;
+
+                string x = curXmlNode.InnerText;
+                curXmlNode = curXmlNode.NextSibling;
+
+                string y = curXmlNode.InnerText;
+                curXmlNode = curXmlNode.NextSibling;
+
+                string width = curXmlNode.InnerText;
+                curXmlNode = curXmlNode.NextSibling;
+
+                string height = curXmlNode.InnerText;
+
+                RestfulScreenData screenData = new RestfulScreenData();
+                screenData.id = id;
+                screenData.x = x;
+                screenData.y = y;
+                screenData.width = width;
+                screenData.height = height;
+
+                screenNodeList.Add(screenData);
+            }
+
+            //启动预监画面服务
+            PreviewImageService.GetInstance().Init(screenNodeList);
+            //启动预监服务器
+            MjpegServer mjpegServer = new MjpegServer(port);
+            mjpegServer.Start();
+
+            string cmd = Console.ReadLine();
+            while (!cmd.Equals("bye"))
+            {
+                cmd = Console.ReadLine();
+            }
+        }
+    }
+}
